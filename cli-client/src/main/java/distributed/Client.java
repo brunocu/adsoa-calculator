787b81e0c147273f21f00039dd2bae8105fbccbd
brunocu <brunocu@msn.com>
2022-09-14 13:47:41 -0500

    NIO Client

--- cli-client/src/main/java/distributed/Client.java
+++ cli-client/src/main/java/distributed/Client.java
@@ -1,20 +1,39 @@
 package distributed;
 
-import distributed.message.ContentCode;
-import distributed.message.Message;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
-import java.util.stream.IntStream;
+import java.util.concurrent.CountDownLatch;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    private final int port;
+    private final CountDownLatch interruptLatch = new CountDownLatch(1);
+
+    public Client(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException, InterruptedException {
+        System.out.println("Calling server on " + port + "...");
+        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress(port));
+        System.out.println("Connected!");
+
+        Thread listenerThread = new Thread(new ClientListener(socketChannel, interruptLatch));
+        Thread promptThread = new Thread(new ClientPrompt(socketChannel, interruptLatch));
+
+        listenerThread.start();
+        promptThread.start();
+
+        // wait for signal to end program
+        interruptLatch.await();
+        System.out.println("Goodbye :)");
+        System.exit(0);  // ugly exit
+//        listenerThread.interrupt();
+//        promptThread.interrupt();
+//        socketChannel.close();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         int port = 50000; // default port
         if (args.length > 0) {
             try {
@@ -23,39 +42,8 @@ public class Client {
                 System.out.println("Bad port name, using default: " + port);
             }
         }
-        System.out.println("Calling server on " + port + "...");
-        try (
-                SocketChannel socketChannel = SocketChannel.open()
-        ) {
-            socketChannel.connect(new InetSocketAddress(port));
-            System.out.println("Connected!");
-            BufferedReader stdIn = new BufferedReader(
-                    new InputStreamReader(System.in)
-            );
-            String userLine;
 
-            while (true) {
-                System.out.print("$ ");
-                userLine = stdIn.readLine();
-                // check for user exit
-                if (userLine.equals("exit"))
-                    break;
-
-                // Build message
-                Message message = new Message(ContentCode.OPERATION, userLine);
-                // Prepare array buffer
-                ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
-                // make space for size Int at beginning of array
-                IntStream.range(0, 4).forEach(i ->
-                        arrayOutputStream.write(0)
-                );
-                ObjectOutputStream outputStream = new ObjectOutputStream(arrayOutputStream);
-                outputStream.writeObject(message);
-                outputStream.close();
-                final ByteBuffer writeByteBuffer = ByteBuffer.wrap(arrayOutputStream.toByteArray());
-                writeByteBuffer.putInt(0, arrayOutputStream.size() - 4);
-                socketChannel.write(writeByteBuffer);
-            }
-        }
+        Client client = new Client(port);
+        client.start();
     }
 }
